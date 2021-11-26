5706355c5675c4adc91daa0626d1b5227152a194
Anton Smith <anton@huge.geek.nz>
2021-11-26 15:18:55 +0100

    initial

---
+++ maas-vm-ping.py
@@ -0,0 +1,26 @@
+import oauth.oauth as oauth
+import httplib2
+import urllib.parse
+import uuid
+
+def perform_API_request(site, uri, method, key, secret, consumer_key):
+    resource_tok_string = "oauth_token_secret=%s&oauth_token=%s" % (
+        secret, key)
+    resource_token = oauth.OAuthToken.from_string(resource_tok_string)
+    consumer_token = oauth.OAuthConsumer(consumer_key, "")
+
+    oauth_request = oauth.OAuthRequest.from_consumer_and_token(
+        consumer_token, token=resource_token, http_url=site,
+        parameters={'oauth_nonce': uuid.uuid4().hex})
+    oauth_request.sign_request(
+        oauth.OAuthSignatureMethod_PLAINTEXT(), consumer_token,
+        resource_token)
+    headers = oauth_request.to_header()
+    url = "%s%s" % (site, uri)
+    http = httplib2.Http()
+    return http.request(url, method, body=None, headers=headers)
+
+# API key = '<consumer_key>:<key>:<secret>'
+response = perform_API_request(
+    'http://server/MAAS/api/1.0', '/nodes/?op=list', 'GET', '<key>', '<secret>',
+    '<consumer_key>')
